3a8fd36777153b917d473c37c30b4d53f0838c8b
Ernest Osipov <mazer-93@mail.ru>
2024-05-22 19:43:09 +0300

    lesson3.py

--- lesson1/lesson3.py
+++ lesson1/lesson3.py
@@ -1,2 +1,2 @@
 user_name = input("inter you name:")
-print("Привет + user_name»")
+print("Привет, "+ user_name,"!")
--- lesson1/lesson4.py
+++ lesson1/lesson4.py
@@ -1,3 +1,3 @@
 first_name = input("enter your first name:")
 last_name = input("enter your last name:")
-print('вас зоыут', last_name, first_name)
+print('вас зовут', last_name, first_name)
